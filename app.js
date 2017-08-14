104248c99dc31c129436b6337615aa71e7f3cc66
MHC User <mhcuser@n21-133.mtholyoke.edu>
2017-08-14 14:56:07 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  //example code - feel free to delete!
+  var timesSillyButtonClicked = 0;
+  $('#silly-button').click(function() {
+    timesSillyButtonClicked++;
+    $('#silly-button-text').text(
+      'You clicked the silly button ' + timesSillyButtonClicked + ' times!',
+    );
+  });
+
+  //write your code here!
+});
