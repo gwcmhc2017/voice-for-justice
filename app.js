b26f1eddebd8ef7927391aa49292098619408644
MHC User <mhcuser@n21-71.mtholyoke.edu>
2017-08-16 15:40:00 -0400

    enter button does not work

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ $(document).ready(function() {
 
 
   $('#result').append('Dear ' + text + ', My name is ' + name  + '. I live in ' + location + '. I am concerned about ' + concern + '. I do not think you are doing enough to combat this issue in America. My community and I would appreciate your efforts to')
-  .append(efforts + '. Thank you for your consideration. Sincerely ' + name + '.<br/>');
+  .append(efforts + '. Thank you for your consideration. Sincerely ' + name + '.<br/>' );
 });
-   $('#storybutton').show('copy');
-
 });
