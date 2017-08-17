d2f0dec643ddf6c43c94c88658f6a70632e6c15a
MHC User <mhcuser@n21-133.mtholyoke.edu>
2017-08-17 15:42:23 -0400

    gross colors

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ $(document).ready(function() {
   var concern=$('#concern').val();
 
 
-  $('#result').append('Dear ' + text + ', My name is ' + name  + '. I live in ' + location + '. I am concerned about ' + concern + '. I do not think you are doing enough to combat this issue in America. My community and I would appreciate your efforts to')
+  $('#result').text('Dear ' + text + ', My name is ' + name  + '. I live in ' + location + '. I am concerned about ' + concern + '. I do not think you are doing enough to combat this issue in America. My community and I would appreciate your efforts to')
   .append(efforts + '. Thank you for your consideration. Sincerely ' + name + '.<br/>' );
+});
+$('#storybutton2').click(function(){
+var person=$('#person').val();
+var place=$('#place').val();
+var issuez=$('#issuez').val();
+var solution=$('#solution').val();
+var contact=$('#contact').val();
+
+$('#result2').text('Hello, my name is ' + person + '. I live at' + place + ". I am concerned about" + issuez + ". I do not think you are doing enough to combat this issue in America. My community and I would appreciate your efforts to " + solution + ". Thank you for your consideration.")
+.append(". You can contact me at" + contact);
 });
 });
